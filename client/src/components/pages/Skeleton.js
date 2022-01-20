65bb76b55719b662798e6769ce119912f8c2a16a
williamyang <willyanglex@gmail.com>
2022-01-20 00:38:36 -0500

    cs edits

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -10,6 +10,8 @@ const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.goo
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
   return (
     <>
+      <h1>Vector</h1>
+      <h2>College App Planning Done Smart</h2>
       {userId ? (
         <GoogleLogout
           clientId={GOOGLE_CLIENT_ID}
@@ -23,24 +25,10 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
           buttonText="Login"
           onSuccess={handleLogin}
           onFailure={(err) => console.log(err)}
+          
         />
       )}
-      <h1>Good luck on your project :)</h1>
-      <h2> What you need to change in this skeleton</h2>
-      <ul>
-        <li>
-          Change the Frontend CLIENT_ID (Skeleton.js) to your team's CLIENT_ID (obtain this at
-          http://weblab.to/clientid)
-        </li>
-        <li>Change the Server CLIENT_ID to the same CLIENT_ID (auth.js)</li>
-        <li>
-          Change the Database SRV (mongoConnectionURL) for Atlas (server.js). You got this in the
-          MongoDB setup.
-        </li>
-        <li>Change the Database Name for MongoDB to whatever you put in the SRV (server.js)</li>
-      </ul>
-      <h2>How to go from this skeleton to our actual app</h2>
-      <a href="http://weblab.to/get-started">Check out this getting started guide</a>
+      
     </>
   );
 };
